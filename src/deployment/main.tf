4bd4d734b4692ffd0888dd02f800093a0e541330
Poorani Kamalakannan <pooranikamalakannan@allianzlife.com>
2021-04-19 14:45:27 +0000

    Updated main.tf

--- src/deployment/main.tf
+++ src/deployment/main.tf
@@ -30,7 +30,7 @@ module "aks_network" {
 module "aks_identities" {
   source       = "../modules/aks_identities"
   cluster_name = var.cluster_name
-  client_id="${module.aks_identities.client_id}"
+  client_id="${module.aks_identities.cluster_client_id}"
   client_secret="${module.aks_identities.client_secret}"
 }
 
