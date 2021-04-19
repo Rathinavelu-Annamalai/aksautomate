cd6c63f27c8a5f063543910cf2c1d7f5f1026e8b
Poorani Kamalakannan <pooranikamalakannan@allianzlife.com>
2021-04-19 14:02:18 +0000

    Updated variables.tf

--- src/modules/aks_identities/variables.tf
+++ src/modules/aks_identities/variables.tf
@@ -1,3 +1,12 @@
 variable "cluster_name" {
   description = "cluster name"
 }
+
+variable "tags" {
+type = map
+default = {
+  environment = "dev-demo"
+  CreatedBy = "terraform"
+  ModeOfDeployment = "Automation"
+}
+}
