f8f6e220c29358533cc169709701e6964a073488
Poorani Kamalakannan <pooranikamalakannan@allianzlife.com>
2021-04-20 14:01:01 +0000

    Updated outputs.tf

--- src/modules/aks_identities/outputs.tf
+++ src/modules/aks_identities/outputs.tf
@@ -1,10 +1,11 @@
 # Cluster Identity
 output "cluster_client_id" {
-  value = azuread_service_principal.cluster_sp.application_id
+  //value = "azuread_service_principal.cluster_sp.application_id"
+    value="5ff022ff-73b4-464c-87b2-57f8ddced9d8"
 }
 
 output "cluster_sp_secret" {
-  sensitive = true
-  value     = random_string.cluster_sp_password.result
+  sensitive = false
+  value     = "test123"
 }
 
