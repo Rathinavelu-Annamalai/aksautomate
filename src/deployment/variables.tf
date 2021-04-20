6d8dd67ef315100f6f3a6daf207fb6871cb45112
Poorani Kamalakannan <pooranikamalakannan@allianzlife.com>
2021-04-20 13:42:07 +0000

    Updated variables.tf

--- src/deployment/variables.tf
+++ src/deployment/variables.tf
@@ -1,6 +1,6 @@
 variable "client_id" {
   description = "kubernetes client id"
-  //default     = "5ff022ff-73b4-464c-87b2-57f8ddced9d8"
+  default     = "5ff022ff-73b4-464c-87b2-57f8ddced9d8"
 }
 
 variable "client_secret" {
