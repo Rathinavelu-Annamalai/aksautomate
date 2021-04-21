53f0be566e7059a8e4d235008c633e49b763fc7f
Poorani Kamalakannan <pooranikamalakannan@allianzlife.com>
2021-04-21 05:57:24 +0000

    Updated outputs.tf

--- src/modules/aks_identities/outputs.tf
+++ src/modules/aks_identities/outputs.tf
@@ -1,11 +1,11 @@
 # Cluster Identity
 output "cluster_client_id" {
-  //value = "azuread_service_principal.cluster_sp.application_id"
-    value="5ff022ff-73b4-464c-87b2-57f8ddced9d8"
+  value = "azuread_service_principal.cluster_sp.application_id"
+   // value="5ff022ff-73b4-464c-87b2-57f8ddced9d8"
 }
 
 output "cluster_sp_secret" {
-  sensitive = false
-  value     = "test123"
+  sensitive = true
+  //value     = "test123"
 }
 
