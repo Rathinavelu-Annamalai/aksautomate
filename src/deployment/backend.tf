1a421afea519ccf9baa1679dabf69eda0007f5e9
Poorani Kamalakannan <pooranikamalakannan@allianzlife.com>
2021-04-21 06:21:28 +0000

    Updated backend.tf

--- src/deployment/backend.tf
+++ src/deployment/backend.tf
@@ -4,8 +4,6 @@ terraform {
 	    container_name       = "terraform"
 	    key                  = "terraform.tfstate"
     	access_key  ="__storagekey__"
-		client_id = "__clientid__"
-		client_secret="__clientsecret__"
-	  features{}
+		features{}
   }
 }
