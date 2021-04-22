ccd1df2e5dc79174ef051d0593de1a8c15f31f46
Poorani Kamalakannan <pooranikamalakannan@allianzlife.com>
2021-04-22 11:08:24 +0000

    Updated main.tf

--- src/deployment/main.tf
+++ src/deployment/main.tf
@@ -35,7 +35,7 @@ data "azuread_service_principal" "aks_principal" {
 resource "azurerm_role_assignment" "acrpull_role" {
   scope                            = azurerm_container_registry.acr.id
   role_definition_name             = "AcrPull"
-  principal_id                     = var.object_id
+  principal_id                     = var.client_id
   skip_service_principal_aad_check = true
 }
 
